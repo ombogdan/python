b9b377750d18996b199b24e831c90ef6e65c2824
ombogdan <omelcenko44@gmail.com>
2020-12-23 14:51:50 +0200

    Changed shapefile.py

--- shapes/main.py
+++ shapes/main.py
@@ -1,27 +1,62 @@
+import json
 import os
 
 import shapefile
 
-all_files = os.listdir("./Ivannucha")
+all_files = os.listdir("./АГРО-ТРЕЙДЕР ПП/Більмачівка/")
 files_dbf = filter(lambda x: x.endswith('.dbf'), all_files)
+result = []
 for dbf in files_dbf:
     new_directory = dbf.replace('.dbf', "").replace("№", "").replace(" ", '')
     # os.mkdir("./newDirectory/")
-    sf = shapefile.Reader("./Ivannucha/" + dbf, encoding='ISO8859-1')
+    sf = shapefile.Reader("./АГРО-ТРЕЙДЕР ПП/Більмачівка/" + dbf, encoding='ISO8859-1')
     # header = ("lon" + "," + "lat" + "," + "val\n")
     k = []
     z = ''
-    # print(new_directory)
-    with shapefile.Reader("./Ivannucha/" + dbf, encoding='ISO8859-1') as shp:
-        K = open('./newDirectory/'+new_directory+".txt", 'w')
+    dict = {}
+    dict["name"] = new_directory
+
+    with shapefile.Reader("./АГРО-ТРЕЙДЕР ПП/Більмачівка/" + dbf, encoding='ISO8859-1') as shp:
+        analysis_list = []
+
         for i in range(len(shp)):
+            analysis = {}
+
             shapes = sf.shape(i)
             points_lon = shapes.points[0][0]
             points_lat = shapes.points[0][1]
             fields = sf.record(i)
-            x = (str(new_directory)+";"+str(points_lon) + "," + str(points_lat) + ";" + str(fields[7])+ ";"+str(fields[8])+";"+str(fields[9])+";"+str(fields[10])+";"+str(fields[11])+";"+str(fields[12])+";"+str(fields[13])+"\n")
-            z = z+ x
-            # print(x)
-        # print(z)
-        K.write(z)
-        # K.write(k)
+            print(fields)
+
+            shape = ("lat=" + str(points_lat) + "," + "lon=" + str(points_lon) + ";" + str(fields[5]) + ";" +
+                     str(fields[6]) + ";" + str(fields[7]) + ";" + str(fields[8]) + ";" + str(fields[9]) + ";" +
+                     str(fields[10]) + ";" + str(fields[11]) + ";" + 'null' + ";" + 'null' + ";" + 'null' + ";" +
+                     'null' + ";" + 'null' + ";" + 'null' + ";" + 'null' + ";" + 'null' + ";" + 'null' + ";" + 'null' + ";" + 'null')
+            # z = z + x
+            analysis["lat"] = str(points_lat)
+            analysis["lon"] = str(points_lon)
+            analysis["k"] = str(fields[4])
+            analysis["no3"] = str(fields[5])
+            analysis["org"] = str(fields[6])
+            analysis["p"] = str(fields[7])
+            analysis["ph"] = str(fields[8])
+            analysis["s"] = str(fields[9])
+            analysis["zn"] = str(fields[10])
+            analysis["ca"] = None
+            analysis["mg"] = None
+            analysis["na"] = None
+            analysis["mn"] = None
+            analysis["cu"] = None
+            analysis["b"] = None
+            analysis["fe"] = None
+            analysis["ss"] = None
+            analysis["bs"] = None
+            analysis["cec"] = None
+            analysis["al"] = None
+            analysis_list.append(analysis)
+
+    dict["analysis"] = analysis_list
+
+    result.append(dict)
+file = open("C:/Users/omelc/Desktop/shapes.txt", "w")
+file.write(json.dumps(result))
