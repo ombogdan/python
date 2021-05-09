5715e776c2c0a6fe86098787bf035ae2fc5b548b
ombogdan <omelcenko44@gmail.com>
2021-05-10 00:32:09 +0300

    added changes

---
+++ parser_xlsx/LandBank/Земельний/ew.py
@@ -0,0 +1,17 @@
+﻿A = []
+file = open('./kadastr_number.txt', encoding='utf8')
+string = file.readlines()
+for str in string:
+    # A.append(str)
+    A.append(str.replace("\n", "").replace("\ufeff", '').replace("\xa0", ''))
+    # print(str)
+# print(A)
+counter = {}
+
+for elem in A:
+    # print(elem)
+    counter[elem] = counter.get(elem, 0) + 1
+
+doubles = {element: count for element, count in counter.items() if count > 1}
+
+print(doubles)
---
+++ sender/logvinenko.py
@@ -0,0 +1,3 @@
+﻿import requests
+message = "JJ"
+requests.get("https://api.telegram.org/bot1368282448:AAHtHafsLIfMKkKGz6ySvmFFpseo_ozEs-c/sendMessage?chat_id=926584029&text={message}".format(message=message))
